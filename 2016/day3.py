f2d0a06d10f8219bb28ac3261dc12d0b47d7f8da
Karl <karlsmith@bouzou.com>
2016-12-09 16:58:23 -0500

    As Willy Wonka would say, 'Wait a minute! Strike that, reverse it. Thank you.'

--- 2016/day3.py
+++ 2016/day3.py
@@ -1,12 +1,29 @@
 import re
+from input_day3 import *
+
+debug = True
 
 def is_valid_triangle(*triangle):
     if type(triangle) is tuple:
         triangle = list(triangle)
         triangle = [list(i) for i in triangle][0]
     triangle.sort()
-    if triangle[2] <= triangle[0] + triangle[1]: return False
-    else: return True
+    if triangle[2] <= triangle[0] + triangle[1]: return True
+    else: return False
 
 def string_to_list(triangles):
-	re.sub
+    tNums = re.findall(
+        r"([0-9]+)",
+        triangles)
+    tNums = [int(x) for x in tNums]
+    tList = []
+    for i in range(len(tNums)/3):
+        tList += [tNums[i*3:i*3+3]]
+    return tList
+
+if __name__ == '__main__':
+    if debug is True: stream = test
+    if debug is False: stream = prod
+    triangles = string_to_list(stream)
+    print "Number of valid triangles: "
+    print len([x for x in triangles where is_valid_triangle(x)])
--- 2016/testsDay3.py
+++ 2016/testsDay3.py
@@ -3,11 +3,19 @@ from day3 import *
 
 class test_day3(unittest.TestCase):
     def test_is_valid_triangle(self):
-        self.assertEqual(is_valid_triangle([2,3,4]), False)
-        self.assertEqual(is_valid_triangle([4,3,2]), False)
-        self.assertEqual(is_valid_triangle([3,2,4]), False)
-        self.assertEqual(is_valid_triangle([1,2,4]), True)
-        self.assertEqual(is_valid_triangle([1,2,10]), True)
+        self.assertEqual(is_valid_triangle([2,3,4]), True)
+        self.assertEqual(is_valid_triangle([4,3,2]), True)
+        self.assertEqual(is_valid_triangle([3,2,4]), True)
+        self.assertEqual(is_valid_triangle([1,2,4]), False)
+        self.assertEqual(is_valid_triangle([1,2,10]), False)
+
+    def test_string_to_list(self):
+        first = '''    4   21  894'''
+        second = '''    4   21  894
+  419  794  987
+  424  797  125'''
+        self.assertEqual(string_to_list(first), [[4,21,894]])
+        self.assertEqual(string_to_list(second), [[4,21,894],[419,794,987],[424,797,125]])
 
 if __name__ == '__main__':
     unittest.main(verbosity=1)
