e3b06f112ecd244bf5fabd2d181c1bc1bea6688a
Karl <karlsmith@bouzou.com>
2018-12-09 22:48:41 -0500

    2018 Day 3: Removes identity functions, tidies up code, removes dup optimization code (it did not improve performance), makes code more readable

--- 2018/day3.js
+++ 2018/day3.js
@@ -1,39 +1,21 @@
 
-
 class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
-  is(other) { return this.x == other.x && this.y == other.y; }
-  add(other) {
-    return new Vector(this.x + other.x, this.y + other.y);
-  }
+  add(other) { return new Vector(this.x + other.x, this.y + other.y); }
 }
 
 class CompositeSurface {
-  constructor(surfaces) {
-    this.surfaces = surfaces;
-    this._remove_dups();
-  }
-  _remove_dups() {
-    for (let i=0; i<this.surfaces.length; null) {
-      const otherSurfs = this.surfaces.slice(i+1,this.length);
-      if (otherSurfs.length > 0 && otherSurfs.every(e => e.is(this.surfaces[i]))) {
-        this.surfaces = [...this.surfaces.slice(0,i), ...this.surfaces.slice(i+1, this.surfaces.length)];
-      } else {
-        i++;
-      }
-    }
-    return this;
-  }
+  constructor(surfaces) { this.surfaces = surfaces; }
   get length() { return this.surfaces.length; }
   get isEmpty() { return this.surfaces.length == 0; }
   add(other) { return new CompositeSurface([...this.surfaces, ...other.surfaces]); }
   overlap(other) {
-    const newSurfacesArrOfArrs = this.surfaces.map(rect => rect.overlap(other))
-      .filter(r => !r.isEmpty).map(s => s.surfaces);
-    const newSurfaces = flatten(newSurfacesArrOfArrs);
+    const newSurfaces = this.surfaces.map(r => r.overlap(other))
+      .filter(s => s.length > 0)
+      .reduce((arr,s) => [...arr, ...s.surfaces], []);
     return new CompositeSurface(newSurfaces);
   }
   get area() {
@@ -41,10 +23,7 @@ class CompositeSurface {
     const first = this.surfaces[0];
     const others = new CompositeSurface(this.surfaces.slice(1,this.surfaces.length));
     const totalOverlap = others.overlap(first);
-    const overlapArea = (totalOverlap != undefined) ? totalOverlap.area : 0;
-    const firstArea = first.area;
-    const othersArea = others.area;
-    return firstArea + othersArea - overlapArea;
+    return first.area + others.area - totalOverlap.area;
   }
 }
 
@@ -55,10 +34,7 @@ class Rect {
     this.height = height;
     this.id = id;
   }
-  is(other) {
-    return this.pos.is(other.pos) && this.width == other.width && this.height == other.height;
-  }
-  get surfaces() { return Array.from([this]); }
+  get surfaces() { return [this]; }
   get left() { return this.pos.x; }
   get right() { return this.pos.x + this.width; }
   get top() { return this.pos.y; }
@@ -68,7 +44,9 @@ class Rect {
   get isEmpty() { return (this.width == 0 && this.height == 0); }
 
   overlap(other) {
-    return new CompositeSurface(other.surfaces.map(s => this.rect_overlap(s)).filter(s => !s.isEmpty));
+    const newSurfaces = other.surfaces.map(s => this.rect_overlap(s))
+      .filter(s => !s.isEmpty);
+    return new CompositeSurface(newSurfaces);
   }
 
   rect_overlap(other) {
@@ -93,19 +71,13 @@ function surface_self_overlapping(rects) {
   return tallySurface;
 }
 
-function flatten(arr) {
-  return [].concat.apply([], arr);
-}
-
 function data_from_puzzle_input() {
   const data = require('./day3_data');
-  const rawDataRegex = /#(\d+) \@ (\d+),(\d+): (\d+)x(\d+)/;
-  const pf = parseFloat;
-  const rects = data.map(d => {
-    const raw = d.match(rawDataRegex);
-    return new Rect(pf(raw[2]),pf(raw[3]),pf(raw[4]),pf(raw[5]),id=pf(raw[1]));
+  const rawDataReg = /#(\d+) \@ (\d+),(\d+): (\d+)x(\d+)/;
+  return data.map(d => {
+    const [_,id,x,y,width,height] = d.match(rawDataReg).map(parseFloat);
+    return new Rect(x,y,width,height,id);
   });
-  return rects;
 }
 
 // let rects = [
@@ -118,10 +90,6 @@ function data_from_puzzle_input() {
 let rects = data_from_puzzle_input();
 
 
-console.assert(new Vector(1,1).is(new Vector(1,1)), 'vector identity');
-console.assert(new Vector(1,1).is(new Vector(1,2)) == false && new Vector(2,1).is(new Vector(1,1)) == false,
-  'vector non-identity');
-
 console.assert(new Rect(2,2,1,3).overlap(new Rect(2,2,1,3)).area == 3, 'overlap identical rect');
 console.assert(new Rect(2,2,1,3).overlap(new Rect(2,2,1,2)).area == 2, 'overlap partial rect');
 console.assert(new Rect(1,3,4,4).overlap(new Rect(3,1,4,4)).area == 4, 'overlap separate rect');
@@ -130,8 +98,6 @@ console.assert(new CompositeSurface([new Rect(3,1,4,4)]).overlap(new Rect(1,3,4,
 console.assert(
   new Rect(1,3,4,4).overlap(new CompositeSurface([new Rect(3,1,4,4), new Rect(1,1,1,1), new Rect(7,7,1,1)])).area == 4,
   'overlap with composite surface containing extra rects');
-console.assert(new Rect(2,2,1,3).is(new Rect(2,2,1,3)), 'rect identity');
-console.assert(new Rect(2,2,1,3).is(new Rect(2,2,1,3)), 'rect non-identity');
 
 const twoTwoOneThree = new CompositeSurface([new Rect(2,2,1,3)]);
 const twoTwoOneTwo = new CompositeSurface([new Rect(2,2,1,2)]);
@@ -155,11 +121,7 @@ console.assert(
   severalIdenticalRects.overlap(severalIdenticalRects).area == 16,
   'overlap identical multiple rect composite surfaces');
 
-console.assert(
-  severalIdenticalRects.surfaces.length == 1,
-  'composite surface constructor removes duplicates');
-
-// const totalOverlap = surface_self_overlapping(rects);
+const totalOverlap = surface_self_overlapping(rects);
 // console.log(`totalOverlap has ${totalOverlap.surfaces.length} rects`);
 // console.log('if this number is large, the calculation will take a long time');
 // console.log('going to calculate area now...');
@@ -167,5 +129,6 @@ console.assert(
 // // confirmed answer for my data: 101781
 
 const totalSurface = new CompositeSurface(rects);
-const freeRect = rects.find((r, i) => rects.every((r2, i2) => i == i2 || r2.overlap(r).isEmpty));
+const freeRect = rects.find((r, i) => rects.every((r2, i2) =>  i == i2 || r2.overlap(r).isEmpty));
 console.log(`Part Two: ${freeRect.id}`);
+// Confirmed answer: 909
